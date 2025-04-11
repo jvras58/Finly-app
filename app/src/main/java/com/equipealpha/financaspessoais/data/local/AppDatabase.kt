9e7a260316499a86eee6b9f63a1c22dbec8abf2a
jvras58 <jvras@cin.ufpe.br>
2025-04-11 15:20:21 -0300

    + Limpeza

--- app/src/main/java/com/equipealpha/financaspessoais/data/local/AppDatabase.kt
+++ app/src/main/java/com/equipealpha/financaspessoais/data/local/AppDatabase.kt
@@ -4,16 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.equipealpha.financaspessoais.data.dao.UserProfileDao
 import com.equipealpha.financaspessoais.data.entities.Transaction
-import com.equipealpha.financaspessoais.data.entities.UserProfile
 import com.equipealpha.financaspessoais.data.local.dao.TransactionDao
-import com.equipealpha.financaspessoais.data.migrations.MIGRATION_1_2
 
-@Database(entities = [Transaction::class, UserProfile::class], version = 2, exportSchema = false)
+@Database(entities = [Transaction::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
-    abstract fun userProfileDao(): UserProfileDao
 
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
@@ -24,9 +20,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "finance_db"
-                )
-                    .addMigrations(MIGRATION_1_2) // Registra a migração 1 para 2
-                    .build()
+                ).build()
                 INSTANCE = instance
                 instance
             }
